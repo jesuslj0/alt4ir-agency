0c38d260a0f41bc13a8ddd15b1f49f0753352c76
xexu <budece13@gmail.com>
2026-06-02 20:02:38 +0100

    fix next xexu

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   transpilePackages: ["@splinetool/react-spline", "@splinetool/runtime"],
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
